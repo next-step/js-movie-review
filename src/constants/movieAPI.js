ba09e164c0a0bfce99cca1e62a9e45d7c6cbb994
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-27 20:34:23 +0900

    feat: movieAPI 상수 추가

---
+++ src/constants/movieAPI.js
@@ -0,0 +1,23 @@
+/* TMDB */
+const TMDB_CONFIG = Object.freeze({
+  GET_MOVIE: {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.MOVIE_API_KEY}`,
+      accept: 'application/json',
+    },
+  },
+});
+
+const TMDB = Object.freeze({
+  POPULAR_MOVIE: {
+    endpoint:
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+    config: TMDB_CONFIG.GET_MOVIE,
+  },
+});
+
+/* Export */
+export const MOVIE_API = Object.freeze({
+  TMDB,
+});
