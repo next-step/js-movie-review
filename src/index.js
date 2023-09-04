8a046813a20580bbf24be1f4fa25d19365366498
ding-co <pangoons@naver.com>
2023-09-04 14:22:35 +0900

    chore: Setup image path and css with webpack config
    Q#

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/reset.css';
+import './styles/common.css';
+
+import './assets/logo.png';
+import './assets/search_button.png';
+import './assets/star_empty.png';
+import './assets/star_filled.png';
