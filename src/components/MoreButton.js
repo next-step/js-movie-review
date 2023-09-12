6a5e6a88bf3e043c6d439a09ede0eb5f9a3ff40f
Gn0lee <jinho605@gmail.com>
2023-09-12 22:43:01 +0900

    MoreButton 구현

--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,1 +1,24 @@
-export default class MoreButton {}
+export default class MoreButton {
+	#element;
+
+	constructor(rootElement, onClick) {
+		this.#element = document.createElement('button');
+		this.#element.classList.add('btn');
+		this.#element.classList.add('primary');
+		this.#element.classList.add('full-width');
+		this.#element.innerText = '더 보기';
+		this.#element.setAttribute('type', 'button');
+
+		this.#element.addEventListener('click', onClick);
+
+		rootElement.appendChild();
+	}
+
+	hideButton() {
+		this.#element.style.display = 'none';
+	}
+
+	showButton() {
+		this.#element.style.display = 'block';
+	}
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,38 +1,38 @@
 import MovieCard from './MovieCard';
 
 export default class MovieList {
-	movies;
-	container;
+	#movies;
+	#container;
 
 	constructor(rootElement) {
-		this.movies = [];
-		this.container = document.createElement('ul');
-		this.container.classList.add('item-list');
+		this.#movies = [];
+		this.#container = document.createElement('ul');
+		this.#container.classList.add('item-list');
 
-		rootElement.appendChild(this.container);
+		rootElement.appendChild(this.#container);
 	}
 
 	renderMovies() {
 		const fragment = document.createDocumentFragment();
 
-		this.movies.forEach(movie => {
+		this.#movies.forEach(movie => {
 			const movieCard = new MovieCard(movie);
 
 			fragment.appendChild(movieCard.element);
 		});
 
-		this.container.appendChild(fragment);
+		this.#container.appendChild(fragment);
 	}
 
 	updateMovies(newMovies) {
-		this.movies = newMovies;
-		this.container.innerHTML = '';
+		this.#movies = newMovies;
+		this.#container.innerHTML = '';
 
 		this.renderMovies();
 	}
 
 	appendMovies(newMovies) {
-		this.movies = [...this.movies, ...newMovies];
+		this.#movies = [...this.#movies, ...newMovies];
 
 		this.renderMovies();
 	}
