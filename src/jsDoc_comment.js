49db805069a09f989023aa85bfaf1b5e17a693d9
Frank Kim <frankuserconnect@gmail.com>
2023-09-12 21:17:58 +0900

    feat: adding jsDoc

---
+++ src/jsDoc_comment.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @typedef {Object} MovieItem
+ * @property {boolean} adult
+ * @property {string} backdrop_path
+ * @property {number[]} genre_ids
+ * @property {number} id
+ * @property {string} original_language
+ * @property {string} original_title
+ * @property {string} overview
+ * @property {number} popularity
+ * @property {string} poster_path
+ * @property {string} release_date
+ * @property {string} title
+ * @property {boolean} video
+ * @property {number} vote_average
+ * @property {number} vote_count
+ */
