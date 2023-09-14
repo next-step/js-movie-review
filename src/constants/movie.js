24312f1a0d278384f8da847cf8d6855869a384f1
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-09-15 08:10:39 +0900

    refactor: 재사용되는 상수 위치 변경

---
+++ src/constants/movie.js
@@ -0,0 +1,2 @@
+const MIN_RATING = 0;
+const MAX_RATING = 10;
--- src/validation/movieValidation.js
+++ src/validation/movieValidation.js
@@ -1,23 +1,15 @@
-const MIN_RATING = 0;
-const MAX_RATING = 10;
-
-const ERROR = Object.freeze({
-  INVALID_STRING: '잘못된 영화제목입니다.',
-  INVALID_URL: '유효한 URL이 아닙니다.',
-  INVALID_RATING: (minRating, maxRating) =>
-    `점수는 ${minRating}점과 ${maxRating}점 사이여야 합니다.`,
-});
+import { ERROR } from '../constants';
 
 const isValidString = (value) => {
   if (!(typeof value === 'string' && value.trim() !== ''))
-    throw new Error(ERROR.INVALID_STRING);
+    throw new Error(ERROR.MOVIE.INVALID_STRING);
 };
 
 const isValidURL = (value) => {
   try {
     new URL(value);
   } catch {
-    throw new Error(ERROR.INVALID_URL);
+    throw new Error(ERROR.MOVIE.INVALID_URL);
   }
 };
 
@@ -25,7 +17,7 @@ const isValidRating = (value) => {
   if (
     !(typeof value === 'number' && MIN_RATING <= value && value <= MAX_RATING)
   )
-    throw new Error(ERROR.INVALID_RATING(MIN_RATING, MAX_RATING));
+    throw new Error(ERROR.MOVIE.INVALID_RATING(MIN_RATING, MAX_RATING));
 };
 
 export const movieValidation = (movieData) => {
