14720a1d3772ace666aad638fc39137fdd5f7ef6
Gn0lee <jinho605@gmail.com>
2023-09-18 13:11:36 +0900

    modal 초기 템플릿 작성

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import App from './App';
 
 import '../templates/reset.css';
 import '../templates/common.css';
+import '../templates/modal.css';
 
 new App();
--- src/lib/HTMLFormat.js
+++ src/lib/HTMLFormat.js
@@ -15,4 +15,32 @@ export const HTMLFormat = Object.freeze({
 				</div>
 			</a>
 	`,
+	MODAL: (title, vote, posterPath, description, genre) => `
+			<div class="modal">
+        <div class="modal-inner">
+        	<div class="modal-header">
+        		<div >${description}</div>
+          	<div class="modal-close"></div>
+					</div>
+          <div class="modal-content-box">
+          	  <img
+								class="modal-item-poster"
+								src="https://image.tmdb.org/t/p/w292_and_h433_face/${posterPath}"
+								loading="lazy"
+								alt="${title}"
+							/>
+							<div class="modal-item-box">
+								<div class="modal-item-genre">
+									<div>${genre}</div>
+									<div class="modal-item-score">
+										<img src="./templates/star_filled.png" alt="별점" />
+										<div>${vote}</div>
+									</div>
+								</div>
+								<div class="modal-item-detail">${description}</div>
+							</div>
+          </div>
+        </div>
+      </div>
+	`,
 });
