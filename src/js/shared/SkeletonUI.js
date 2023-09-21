4035566847feb0bf42127f84d36dd9b30b500823
Frank Kim <frankuserconnect@gmail.com>
2023-09-21 23:42:38 +0900

    improve: Separating Skeleton UI class

--- src/js/shared/SkeletonUI.js
+++ src/js/shared/SkeletonUI.js
@@ -1,18 +1,34 @@
-const renderSkeleton = () => {
-  return ` 
-      <li class="skeleton-container">
-        <a href="#">
-          <div class="item-card">
-            <div class="item-thumbnail skeleton"></div>
-            <div class="item-title skeleton"></div>
-            <div class="item-score skeleton"></div>
-          </div>
-        </a>
-      </li>
-     `;
-};
+import { $createElement } from "../utils/dom";
 
-const SkeletonUI = num =>
-  Array.from({ length: num }, () => renderSkeleton()).join("");
+class SkeletonUI {
+  constructor(count) {
+    const $ul = $createElement("ul", this.renderSkeleton(count));
+    $ul.classList.add("skeleton-cards", "item-list", "absolute");
+    this.$ul = $ul;
+  }
 
-export { SkeletonUI };
+  renderSkeleton(count) {
+    const skeletonCard = `
+      <li>
+        <a href="#"> 
+          <div class="item-card"> 
+            <div class="item-thumbnail skeleton"></div> 
+            <div class="item-title skeleton"></div> 
+            <div class="item-score skeleton"></div> 
+          </div> 
+        </a> 
+      </li>`.repeat(count);
+
+    return skeletonCard;
+  }
+
+  showSkeleton() {
+    this.$ul.classList.remove("hidden");
+  }
+
+  hideSkeleton() {
+    this.$ul.classList.add("hidden");
+  }
+}
+
+export default SkeletonUI;
