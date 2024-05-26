4da38363b264c9305bbb0093b6da87def628b421
ChanwooPark <chanw9503@gmail.com>
2024-05-27 01:36:42 +0900

    Feat. Skeleton 처리 추가

--- src/js/index.js
+++ src/js/index.js
@@ -2,22 +2,48 @@ import '../styles/reset.css';
 import '../styles/common.css';
 import { getMovieList } from '../apis';
 import starFilled from '../assets/star_filled.png';
-import starEmpty from '../assets/star_empty.png';
 
 const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
 
 let pages = 1;
 
 const moreMovies = document.getElementById('more-movies');
+const skeletonMovies = document.querySelector('.skeleton-list');
 const movieList = document.querySelector('.item-list');
+
+function renderSkeletonMovies(flag) {
+  if (flag === true) {
+    const skeletonHTML = Array(20)
+      .fill(
+        `            
+      <li>
+        <a href="#">
+          <div class="item-card">
+            <div class="item-thumbnail skeleton"></div>
+            <div class="item-title skeleton"></div>
+            <div class="item-score skeleton"></div>
+          </div>
+        </a>
+      </li>`
+      )
+      .join(''); // 배열을 문자열로 변환
+
+    skeletonMovies.innerHTML = skeletonHTML;
+  } else {
+    skeletonMovies.innerHTML = '';
+  }
+}
 
 async function renderMovieItems(pages) {
   try {
+    renderSkeletonMovies(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const itemList = await getMovieList(pages);
-    console.log('itemList', itemList);
-    const newItemsHTML = itemList
-      .map((movieInfo) => {
-        return `
+    if (itemList.success !== false) {
+      const newItemsHTML = itemList.results
+        .map((movieInfo) => {
+          return `
         <li>
           <a href="#">
             <div class="item-card">
@@ -35,11 +61,16 @@ async function renderMovieItems(pages) {
           </a>
         </li>
       `;
-      })
-      .join('');
-
-    movieList.innerHTML += newItemsHTML;
-  } catch (error) {}
+        })
+        .join('');
+      movieList.innerHTML += newItemsHTML;
+    } else {
+      moreMovies.style.display = 'none';
+    }
+  } catch (error) {
+  } finally {
+    renderSkeletonMovies(false);
+  }
 }
 
 addEventListener('DOMContentLoaded', () => {
