c77a2a435bb1414ef232f964ebd444785a265c1f
ChanwooPark <chanw9503@gmail.com>
2024-05-27 01:11:09 +0900

    Feat. page에 따른 Movie 정보 불러오기

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,9 +1,9 @@
 const baseURL = 'https://api.themoviedb.org/3//movie/popular';
 
-export async function getMovieList() {
+export async function getMovieList(page) {
   try {
     const url = new URL(baseURL);
-    const params = { api_key: process.env.TMDB_API_KEY };
+    const params = { api_key: process.env.TMDB_API_KEY, page: page };
     url.search = new URLSearchParams(params).toString();
 
     const res = await fetch(url).then((res) => res.json());
