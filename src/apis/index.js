d639b84da0a1963510e88603c9fc07ef2b0fe0ca
ChanwooPark <chanw9503@gmail.com>
2024-05-27 00:54:51 +0900

    Feat. 서버 호출 부분 분리하기

---
+++ src/apis/index.js
@@ -0,0 +1,15 @@
+const baseURL = 'https://api.themoviedb.org/3//movie/popular';
+
+export async function getMovieList() {
+  try {
+    const url = new URL(baseURL);
+    const params = { api_key: process.env.TMDB_API_KEY };
+    url.search = new URLSearchParams(params).toString();
+
+    const res = await fetch(url).then((res) => res.json());
+
+    return res.results;
+  } catch (error) {
+    console.log(error);
+  }
+}
