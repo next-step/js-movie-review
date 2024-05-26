cfe96b49f20d33d9f319e5faa4ed3de25c5defba
ChanwooPark <chanw9503@gmail.com>
2024-05-27 01:11:23 +0900

    Feat.더보기 클릭시 영화정보 추가로 불러오기

--- src/js/index.js
+++ src/js/index.js
@@ -6,37 +6,47 @@ import starEmpty from '../assets/star_empty.png';
 
 const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
 
-const pages = 0;
+let pages = 1;
 
-async function renderMovieItems() {
-  const movieList = document.querySelector('.item-list');
+const moreMovies = document.getElementById('more-movies');
+const movieList = document.querySelector('.item-list');
 
-  const itemList = await getMovieList();
+async function renderMovieItems(pages) {
+  try {
+    const itemList = await getMovieList(pages);
+    console.log('itemList', itemList);
+    const newItemsHTML = itemList
+      .map((movieInfo) => {
+        return `
+        <li>
+          <a href="#">
+            <div class="item-card">
+              <img
+                class="item-thumbnail"
+                src="${imageBaseURL}${movieInfo.poster_path}"
+                loading="lazy"
+                alt="${movieInfo.title}"
+              />
+              <p class="item-title">${movieInfo.title}</p>
+              <p class="item-score">
+              <img src="${starFilled}" alt="별점" /> ${movieInfo.vote_average}
+              </p>
+            </div>
+          </a>
+        </li>
+      `;
+      })
+      .join('');
 
-  movieList.innerHTML = itemList
-    .map((movieInfo) => {
-      return `
-      <li>
-        <a href="#">
-          <div class="item-card">
-            <img
-              class="item-thumbnail"
-              src="${imageBaseURL}${movieInfo.poster_path}"
-              loading="lazy"
-              alt="${movieInfo.title}"
-            />
-            <p class="item-title">${movieInfo.title}</p>
-            <p class="item-score">
-            <img src="${starFilled}" alt="별점" /> ${movieInfo.vote_average}
-            </p>
-          </div>
-        </a>
-      </li>
-    `;
-    })
-    .join('');
+    movieList.innerHTML += newItemsHTML;
+  } catch (error) {}
 }
 
 addEventListener('DOMContentLoaded', () => {
-  renderMovieItems();
+  renderMovieItems(pages);
+});
+
+moreMovies.addEventListener('click', () => {
+  pages += 1;
+  renderMovieItems(pages);
 });
