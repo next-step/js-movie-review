a50b6f4a930abe46ba63eb75ddecebd1b902b1fb
ChanwooPark <chanw9503@gmail.com>
2024-05-27 00:55:12 +0900

    Feat. movies render함수 생성하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
+import '../styles/reset.css';
 import '../styles/common.css';
+import { getMovieList } from '../apis';
+import starFilled from '../assets/star_filled.png';
+import starEmpty from '../assets/star_empty.png';
+
+const imageBaseURL = 'https://image.tmdb.org/t/p/w500';
+
+const pages = 0;
+
+async function renderMovieItems() {
+  const movieList = document.querySelector('.item-list');
+
+  const itemList = await getMovieList();
+
+  movieList.innerHTML = itemList
+    .map((movieInfo) => {
+      return `
+      <li>
+        <a href="#">
+          <div class="item-card">
+            <img
+              class="item-thumbnail"
+              src="${imageBaseURL}${movieInfo.poster_path}"
+              loading="lazy"
+              alt="${movieInfo.title}"
+            />
+            <p class="item-title">${movieInfo.title}</p>
+            <p class="item-score">
+            <img src="${starFilled}" alt="별점" /> ${movieInfo.vote_average}
+            </p>
+          </div>
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+addEventListener('DOMContentLoaded', () => {
+  renderMovieItems();
+});
