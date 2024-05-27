8d91fea5cc8cd41ea7edaa0180eb106cee320bca
ChanwooPark <chanw9503@gmail.com>
2024-05-28 00:58:11 +0900

    Feat. 검색 기능 추가

--- src/apis/getSearchedMovies.js
+++ src/apis/getSearchedMovies.js
@@ -1,13 +1,26 @@
-import ApiClient from './apiClient';
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
 async function getSearchedMovies(query) {
-    const res = ApiClient.request('GET', '3/search/movie', {
-        query: query,
-        api_key: process.env.TMDB_API_KEY,
-        language: 'ko-KR'
-    });
+    try {
+        const url = new URL(BASE_URL);
+        const params = { query: query, api_key: process.env.TMDB_API_KEY };
+        url.search = new URLSearchParams(params).toString();
 
-    return res;
+        const res = await fetch(url, {
+            method: 'GET'
+        }).then((res) => res.json());
+
+        if (res.success === false) {
+            const error = new Error();
+            error.name = res.status_code;
+            error.message = res.status_message;
+            throw error;
+        }
+
+        return res;
+    } catch (error) {
+        console.log();
+    }
 }
 
 export default getSearchedMovies;
