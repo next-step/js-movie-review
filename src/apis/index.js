c2ab37cf1f73c9db096862f7f217690aeb8221e0
chanwoo <chanw9503@gmail.com>
2024-05-27 14:52:02 +0900

    Feat. API 호출시 발생하는 Error에 대한 핸들링 추가

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,14 +1,24 @@
-const baseURL = 'https://api.themoviedb.org/3//movie/popular';
+const BASE_URL = 'https://api.themoviedb.org/3//movie/popular';
 
 export async function getMovieList(page) {
-  try {
-    const url = new URL(baseURL);
-    const params = { api_key: process.env.TMDB_API_KEY, page: page };
-    url.search = new URLSearchParams(params).toString();
+    try {
+        const url = new URL(BASE_URL);
+        const params = { api_key: process.env.TMDB_API_KEY, page: page, language: 'ko-KR' };
+        url.search = new URLSearchParams(params).toString();
 
-    const res = await fetch(url).then((res) => res.json());
-    return res;
-  } catch (error) {
-    console.log(error);
-  }
+        const res = await fetch(url, {
+            method: 'GET'
+        }).then((res) => res.json());
+
+        if (res.success === false) {
+            const error = new Error();
+            error.name = res.status_code;
+            error.message = res.status_message;
+            throw error;
+        }
+
+        return res;
+    } catch (error) {
+        console.log('error', error);
+    }
 }
