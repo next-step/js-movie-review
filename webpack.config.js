d45985f5ed698fb08eda0ff16c9b41de71ed907a
chanwoo <chanw9503@gmail.com>
2024-05-27 16:20:27 +0900

    Design. prettier 적용

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
 
 module.exports = {
-  entry: ['./src/js/index.js'],
-  mode: 'development',
-  resolve: {
-    extensions: ['.js'],
-  },
-  devServer: {
-    static: './dist',
-    open: true,
-    historyApiFallback: true,
-  },
-  output: {
-    publicPath: '/',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new DotEnv(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+    entry: ['./src/js/index.js'],
+    mode: 'development',
+    resolve: {
+        extensions: ['.js']
+    },
+    devServer: {
+        static: './dist',
+        open: true,
+        historyApiFallback: true
+    },
+    output: {
+        publicPath: '/',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new DotEnv()
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
 };
