8fd0c10053f6d130e7b7b9d77a27c1033dcd72d2
BYEONGWANGJIN <F202311010@fresheasy.co.kr>
2024-05-28 20:13:36 +0900

    - feat : cypress 추가

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,7 @@ async function getMovieList({ page }) {
   try {
     // https://api.themoviedb.org/3/movie/popular?api_key=252a4eaee3a54b8671ca4de1dd773f8d&page=1&language=ko-KR&sort_by=popularity.desc
     const response = await fetch(
-      // `${TMDB_BASE_URL}?api_key=${apiKey.api_key}&page=${page}&language=ko-KR&sort_by=popularity.desc`
-      ``
+      `${TMDB_BASE_URL}?api_key=${apiKey.api_key}&page=${page}&language=ko-KR&sort_by=popularity.desc`
     );
     const jsonData = await response.json();
 
