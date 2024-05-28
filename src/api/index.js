df330811f54cb27f1e750d88b594dd5618249e20
BYEONGWANGJIN <F202311010@fresheasy.co.kr>
2024-05-28 20:40:24 +0900

    - feat : .env 파일 적용

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,11 @@
-import apiKey from "../const/api-key";
-
 const TMDB_BASE_URL = "https://api.themoviedb.org/3/movie/popular";
 
 async function getMovieList({ page }) {
   try {
     // https://api.themoviedb.org/3/movie/popular?api_key=252a4eaee3a54b8671ca4de1dd773f8d&page=1&language=ko-KR&sort_by=popularity.desc
+    console.log("process.env.TMDB_API_KEY", process.env.TMDB_API_KEY);
     const response = await fetch(
-      `${TMDB_BASE_URL}?api_key=${apiKey.api_key}&page=${page}&language=ko-KR&sort_by=popularity.desc`
+      `${TMDB_BASE_URL}?api_key=${process.env.TMDB_API_KEY}&page=${page}&language=ko-KR&sort_by=popularity.desc`
     );
     const jsonData = await response.json();
 
