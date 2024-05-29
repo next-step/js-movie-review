08f1684a314b46e1f2d228bba0a4ba2028e469e2
Vespucci95 <donglonk@naver.com>
2024-05-30 00:50:48 +0900

    refactor: 코드 정리

--- src/components/CTAButton.js
+++ src/components/CTAButton.js
@@ -2,6 +2,7 @@ const CTAButton = ({ text }) => {
   const $el = document.createElement('button');
   $el.className = 'btn primary full-width';
   $el.textContent = text;
+
   return $el;
 };
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,9 @@
-export const MovieCard = ({ title, poster, score, icon }) => {
+const MovieCard = ({ title, poster, score, icon }) => {
   const $el = document.createElement('li');
 
   $el.insertAdjacentHTML(
     'afterbegin',
     `
-    <a href="#">
       <div class="item-card">
         <img
           class="item-thumbnail"
@@ -16,9 +15,10 @@ export const MovieCard = ({ title, poster, score, icon }) => {
         <p class="item-title">${title}</p>
         <p class="item-score">${score}<img src="${icon}" alt="별점" /></p>
       </div>
-    </a>
   `
   );
 
   return $el;
 };
+
+export default MovieCard;
