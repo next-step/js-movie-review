5f15535363f3725fb9fc8dfc2a862caec12412bf
Vespucci95 <donglonk@naver.com>
2024-05-29 23:46:00 +0900

    test: 영화 목록 API 테스트 추가
    인기 영화목록 1페이지를 조회하면 20개의 목록을 반환한다.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const context = describe;
+
+describe('영화목록 API 테스트', () => {
+  context('인기 영화목록 1페이지를 조회하면', () => {
+    it('20개의 목록을 반환한다.', () => {
+      const baseUrl = 'https://api.themoviedb.org/3/movie/popular';
+      const param = new URLSearchParams({
+        api_key: Cypress.env('API_KEY'),
+        language: 'ko-KR',
+        page: 1,
+      });
+
+      cy.request('GET', `${baseUrl}?${param}`).as('popularMovies');
+
+      cy.get('@popularMovies').its('status').should('eq', 200);
+      cy.get('@popularMovies').its('body.results').should('have.length', 20);
+    });
+  });
+});
