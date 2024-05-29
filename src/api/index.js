a8ca2473ab87be52d77719908201be1b465b7397
BYEONGWANGJIN <F202311010@fresheasy.co.kr>
2024-05-29 21:28:07 +0900

    feat: e2e테스트 작성

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,8 @@
-const TMDB_BASE_URL = "https://api.themoviedb.org/3/movie/popular";
-
 async function getMovieList({ page }) {
   try {
     // https://api.themoviedb.org/3/movie/popular?api_key=252a4eaee3a54b8671ca4de1dd773f8d&page=1&language=ko-KR&sort_by=popularity.desc
-    console.log("process.env.TMDB_API_KEY", process.env.TMDB_API_KEY);
     const response = await fetch(
-      `${TMDB_BASE_URL}?api_key=${process.env.TMDB_API_KEY}&page=${page}&language=ko-KR&sort_by=popularity.desc`
+      `${process.env.TMDB_BASE_URL}?page=${page}&api_key=${process.env.TMDB_API_KEY}&language=ko-KR&sort_by=popularity.desc`
     );
     const jsonData = await response.json();
 
@@ -15,4 +12,4 @@ async function getMovieList({ page }) {
   }
 }
 
-export { TMDB_BASE_URL, getMovieList };
+export { getMovieList };
