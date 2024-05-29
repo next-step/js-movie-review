da801974b658132bd0ce33ca7021bc2c251f547b
BYEONGWANGJIN <F202311010@fresheasy.co.kr>
2024-05-29 21:33:53 +0900

    feat : 에러 발생시 alert 창 띄워줌

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ async function run() {
     });
     render.footer({ selector: "section", onClick: footerClick });
   } catch (error) {
-    console.error("error", error);
+    alert("에러가 발생했습니다.");
   }
 }
 
