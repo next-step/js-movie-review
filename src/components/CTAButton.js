ff6b0e3671e40cbdd2cfdbafd626b0f045eae45c
Vespucci95 <donglonk@naver.com>
2024-05-30 00:50:06 +0900

    refactor: 버튼 컴포넌트 분리

---
+++ src/components/CTAButton.js
@@ -0,0 +1,8 @@
+const CTAButton = ({ text }) => {
+  const $el = document.createElement('button');
+  $el.className = 'btn primary full-width';
+  $el.textContent = text;
+  return $el;
+};
+
+export default CTAButton;
