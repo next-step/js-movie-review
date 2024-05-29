08a30245e1ec0375f0fa79ad0a6a77c79156d479
Vespucci95 <donglonk@naver.com>
2024-05-30 01:04:22 +0900

    feat: PageHandler 추가

---
+++ src/utils/PageHandler.js
@@ -0,0 +1,24 @@
+const INITIAL_VALUE = {
+  page: 1,
+  size: 20,
+  totalPages: 500,
+};
+
+function PageEventHandler() {
+  let attr = { ...INITIAL_VALUE };
+
+  return {
+    next() {
+      attr.page = attr.page + 1;
+    },
+    getCurrentPage() {
+      return attr.page;
+    },
+    reset() {
+      attr = { ...INITIAL_VALUE };
+    },
+  };
+}
+
+const PageHandler = PageEventHandler();
+export default PageHandler;
