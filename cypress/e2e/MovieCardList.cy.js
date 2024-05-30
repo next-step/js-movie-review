76f24c2274283f38ad42319b1365c4631cb3a795
Paik <gwbaik9717@icloud.com>
2024-05-30 10:32:48 +0900

    test: 로딩시 skeleton ui가 보이는 경우에 대한 테스트 코드 추가

--- cypress/e2e/MovieCardList.cy.js
+++ cypress/e2e/MovieCardList.cy.js
@@ -1,6 +1,9 @@
+import Api from "../../src/js/domain/Api";
+
 const selectors = {
   "item-card": ".item-card",
   "show-more": ".show-more",
+  "skeleton-card": ".skeleton-card",
 };
 
 describe("영화 카드 목록 기능 테스트", () => {
@@ -15,5 +18,23 @@ describe("영화 카드 목록 기능 테스트", () => {
   it("더보기 버튼을 클릭하면 20개의 영화 카드가 추가로 보여야 한다.", () => {
     cy.get(selectors["show-more"]).click();
     cy.get(selectors["item-card"]).should("have.length", 40);
+  });
+
+  it("로딩 중일 때 스켈레톤 카드가 보인다.", () => {
+    // given
+    cy.intercept(Api.generateUrl(2), (req) => {
+      req.continue((res) => {
+        // delay the response for 1 second
+        res.setDelay(1000);
+      });
+    }).as("getMovies");
+
+    // when
+    cy.get(selectors["show-more"]).click(); // fetch next page
+
+    // then
+    cy.get(selectors["skeleton-card"]).should("be.visible");
+    cy.wait("@getMovies");
+    cy.get(selectors["skeleton-card"]).should("not.exist");
   });
 });
