d7ac77a986ca7ea4608007ea31e6ee6019067625
Vespucci95 <donglonk@naver.com>
2024-05-30 18:26:44 +0900

    style: 임시 코드 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/common.css';
 import Layout from './components/Layout';
 import { MovieListView } from './view/MovieView';
 
+window.alert('helloworld!');
 const app = document.getElementById('app');
 app.appendChild(Layout());
 addEventListener('DOMContentLoaded', MovieListView);
