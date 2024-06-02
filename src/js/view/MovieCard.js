520bb35b9e4bf8dbbdd72393c6d72b6fe843205b
Paik <gwbaik9717@icloud.com>
2024-06-02 10:51:40 +0900

    refactor: movie 구조분해할당 적용

--- src/js/view/MovieCard.js
+++ src/js/view/MovieCard.js
@@ -2,18 +2,19 @@ import { createElement } from "../../utils/dom.js";
 
 const MovieCard = {
   generateMovieCard(movie) {
+    const { title, rating, thumbnail } = movie;
     const card = createElement("li");
     card.innerHTML = /* html */ `
     <a href="#">
     <div class="item-card">
       <img
         class="item-thumbnail"
-        src=${movie.thumbnail}
+        src=${thumbnail}
         loading="lazy"
-        alt=${movie.title}
+        alt=${title}
       />
-      <p class="item-title">${movie.title}</p>
-      <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${movie.rating}</p>
+      <p class="item-title">${title}</p>
+      <p class="item-score"><img src="./star_filled.png" alt="별점" /> ${rating}</p>
     </div>
   </a>
     `;
