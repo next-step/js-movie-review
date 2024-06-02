4491031fa8c1759b289ce4f1670c08a5525ca7dc
DongHyeonWon <dhe7700@naver.com>
2024-06-02 23:52:08 +0900

    text: 스켈레톤 ui test 추가

--- cypress/e2e/movie.cy.js
+++ cypress/e2e/movie.cy.js
@@ -1,3 +1,5 @@
+import END_POINT from "../../src/constants/api";
+
 describe("template spec", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8081");
@@ -11,5 +13,19 @@ describe("template spec", () => {
     cy.get(".item-list li").should("have.length", 20);
     cy.contains("더 보기").click();
     cy.get(".item-list li").should("have.length", 40);
+  });
+
+  it("더보기 클릭 후 영화 목록을 불러오기 전 스켈레톤 UI를 보여준다.", () => {
+    cy.intercept(Cypress.env("API_URL") + END_POINT.POPULAR_MOVIES(2), (req) => {
+      req.continue((res) => {
+        res.setDelay(1000);
+      });
+    }).as("getMovies");
+
+    cy.contains("더 보기").click();
+
+    cy.get(".skeleton-box").should("be.visible");
+    cy.wait("@getMovies");
+    cy.get(".skeleton-box").should("not.exist");
   });
 });
