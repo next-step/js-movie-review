67b893ad74dd164fa3fc9f01b7c07759ed3682ea
ChanwooPark <chanw9503@gmail.com>
2024-06-03 01:29:26 +0900

    Fix. SearchBox 필요 컴포넌트 불러오도록 수정

--- src/js/view/seachBox.js
+++ src/js/view/seachBox.js
@@ -1,8 +1,9 @@
-export function renderSearchBox() {
-    return `
-        <div class="search-box">
-        <input id="search-input" type="text" placeholder="검색" />
-        <button class="search-button">검색</button>
-    </div>
-        `;
+export function SearchBox({ input, button }) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('search-box');
+
+    wrapper.append(input);
+    wrapper.append(button);
+
+    return wrapper;
 }
