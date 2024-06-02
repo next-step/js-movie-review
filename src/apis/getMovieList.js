e31db7db479aaf8031386b512e6687546c61005f
ChanwooPark <chanw9503@gmail.com>
2024-06-02 22:07:38 +0900

    Fix. 에러 핸들링 분리(호출부분에서 처리하도록 변경)

--- src/apis/getMovieList.js
+++ src/apis/getMovieList.js
@@ -1,13 +1,25 @@
-import ApiClient from './apiClient';
+import { BASE_URL } from '../constants/api';
+import { APIError } from './error';
+import { createUrlSeacrhParams } from './utils/createUrlSeachParams';
 
 async function getMovieList(page) {
-    const res = ApiClient.request('GET', '3/movie/popular', {
-        api_key: process.env.TMDB_API_KEY,
-        page: page,
-        language: 'ko-KR'
+    const url = createUrlSeacrhParams({
+        baseUrl: BASE_URL,
+        params: { api_key: process.env.TMDB_API_KEY, page: page, language: 'ko-KR' }
     });
 
-    return res;
+    const res = await fetch(url, {
+        method: 'GET'
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            if (data.success !== undefined && !data.success) throw new APIError(data.status_code, data.status_message);
+            return data;
+        });
+
+    return res.results;
 }
 
 export default getMovieList;
