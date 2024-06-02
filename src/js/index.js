5731a09518684cdff709f3d2fca44647bd263e05
ChanwooPark <chanw9503@gmail.com>
2024-06-03 01:42:58 +0900

    ing. 수정중

--- src/js/index.js
+++ src/js/index.js
@@ -1,86 +1,132 @@
 import '../styles/reset.css';
 import '../styles/common.css';
-import getMovieList from '../apis/getMovieList';
-import { Modal } from './view/modal';
-import { covertError } from './domain/error';
-import { Movie } from './domain/movie';
-import { Cinema } from './view/cinema';
-import { renderHeader } from './view/header';
-import { renderLogo } from './view/logo';
-import { renderSearchBox } from './view/seachBox';
+
+import { Modal } from './components/modal';
+
 import getSearchedMovies from '../apis/getSearchEdMovies';
-import renderTitle from './view/title';
+import renderTitle from './components/title';
+import { Cinema } from './components/cinema';
+import Title from './components/title';
+import LogoImg from '../assets/logo.png';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { Logo } from './components/logo';
+import { Header } from './components/header';
+import { SearchBox } from './components/seachBox';
+import getMovieList from '../apis/getMovieList';
 
 const moreMovies = document.getElementById('more-movies');
+const appElement = document.querySelector('.app');
 
 const cinema = new Cinema();
 
 async function renderMovieItems(pages) {
     try {
-        cinema.showSkeleton(true);
+        // cinema.showSkeleton(true);
         const itemList = await getMovieList(pages);
-        if (itemList.success !== false) {
-            const newMoveList = itemList.results.map((item) => new Movie(item));
-            cinema.setMovies(newMoveList);
-            cinema.showMovies();
-        } else {
-            moreMovies.style.display = 'none';
-        }
+        console.log('itemList', itemList);
+        cinema.showMovies(itemList);
     } catch (error) {
         const modal = new Modal();
-        modal.content = `${covertError(error.name)}`;
+        modal.content = error.message;
         document.querySelector('body').appendChild(modal.rendered);
         moreMovies.style.display = 'none';
     } finally {
-        cinema.showSkeleton(false);
+        // cinema.showSkeleton(false);
     }
 }
 
+const handleSeacrhClick = async () => {
+    try {
+        if (searchInput.value) {
+            const res = await getSearchedMovies(searchInput.value);
+            const newMoveList = res.results.map((item) => new Movie(item));
+            cinema.resetMovies();
+            renderTitle(`"${searchInput.value}" 검색 결과`);
+            cinema.showMovies(newMoveList);
+        }
+    } catch (error) {
+        const modal = new Modal();
+        modal.content = '';
+        document.querySelector('body').appendChild(modal.rendered);
+        moreMovies.style.display = 'none';
+    }
+};
+
 addEventListener('DOMContentLoaded', () => {
-    renderHeader(renderLogo(), renderSearchBox());
+    let value = '';
+
+    const onChange = (event) => {
+        event.preventDefault();
+        value = event.target.value;
+        console.log('Current search value:', value);
+    };
+
+    const itemVlews = document.querySelector('.item-view');
+    const headerElement = Header(
+        Logo({ src: LogoImg, alt: '로고' }),
+        SearchBox({
+            input: Input({
+                id: 'search-input',
+                type: 'text',
+                placeholder: '검색',
+                value: value,
+                onChange: onChange
+            }),
+            button: Button({
+                classNames: ['search-button'],
+                name: '검색',
+                type: 'text'
+            })
+        })
+    );
+    itemVlews.appendChild(Title('지금 인기 있는 영화'));
+    itemVlews.appendChild(
+        Button({
+            classNames: ['btn', 'primary', 'full-width'],
+            type: 'button',
+            id: 'more-movies',
+            name: '더 보기'
+        })
+    );
+    document.body.insertBefore(headerElement, document.body.firstChild);
     renderMovieItems(cinema.page);
-    renderTitle('지금 인기 있는 영화');
 
-    const searchButton = document.querySelector('.search-button');
-    const searchInput = document.getElementById('search-input');
+    // searchButton.addEventListener('click', async () => {
+    //     try {
+    //         if (searchInput.value) {
+    //             const res = await getSearchedMovies(searchInput.value);
+    //             const newMoveList = res.results.map((item) => new Movie(item));
+    //             cinema.resetMovies();
+    //             renderTitle(`"${searchInput.value}" 검색 결과`);
+    //             cinema.showMovies(newMoveList);
+    //         }
+    //     } catch (error) {
+    //         const modal = new Modal();
+    //         modal.content = '';
+    //         document.querySelector('body').appendChild(modal.rendered);
+    //         moreMovies.style.display = 'none';
+    //     }
+    // });
 
-    searchButton.addEventListener('click', async () => {
-        try {
-            if (searchInput.value) {
-                const res = await getSearchedMovies(searchInput.value);
-                const newMoveList = res.results.map((item) => new Movie(item));
-                cinema.resetMovies();
-                renderTitle(`"${searchInput.value}" 검색 결과`);
-                cinema.setMovies(newMoveList);
-                cinema.showMovies();
-            }
-        } catch (error) {
-            const modal = new Modal();
-            modal.content = `${covertError(error.name)}`;
-            document.querySelector('body').appendChild(modal.rendered);
-            moreMovies.style.display = 'none';
-        }
-    });
-
-    searchInput.addEventListener('keydown', async ({ key }) => {
-        if (key === 'Enter') {
-            try {
-                if (searchInput.value) {
-                    const res = await getSearchedMovies(searchInput.value);
-                    const newMoveList = res.results.map((item) => new Movie(item));
-                    cinema.resetMovies();
-                    renderTitle(`"${searchInput.value}" 검색 결과`);
-                    cinema.setMovies(newMoveList);
-                    cinema.showMovies();
-                }
-            } catch (error) {
-                const modal = new Modal();
-                modal.content = `${covertError(error.name)}`;
-                document.querySelector('body').appendChild(modal.rendered);
-                moreMovies.style.display = 'none';
-            }
-        }
-    });
+    // searchInput.addEventListener('keydown', async ({ key }) => {
+    //     if (key === 'Enter') {
+    //         try {
+    //             if (searchInput.value) {
+    //                 const res = await getSearchedMovies(searchInput.value);
+    //                 const newMoveList = res.results.map((item) => new Movie(item));
+    //                 cinema.resetMovies();
+    //                 renderTitle(`"${searchInput.value}" 검색 결과`);
+    //                 cinema.showMovies(newMoveList);
+    //             }
+    //         } catch (error) {
+    //             const modal = new Modal();
+    //             modal.content = '';
+    //             document.querySelector('body').appendChild(modal.rendered);
+    //             moreMovies.style.display = 'none';
+    //         }
+    //     }
+    // });
 });
 
 moreMovies.addEventListener('click', () => {
