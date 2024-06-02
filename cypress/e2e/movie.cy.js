46c4dd252cfc0f96ba1447e04321c90bc8f495e4
seungdeok <akk9132@naver.com>
2024-06-03 00:59:53 +0900

    feat: 더보기 버튼을 누르면 그 다음의 영화 목록 20개를 불러온다.

--- cypress/e2e/movie.cy.js
+++ cypress/e2e/movie.cy.js
@@ -6,4 +6,9 @@ describe("영화 리뷰 테스트", () => {
   it("마운트 시 영화 목록의 1페이지를 불러온다.", () => {
     cy.get(".item-list li").should("have.length", 20);
   });
+
+  it("더보기 버튼을 누르면 그 다음의 영화 목록 20개를 불러온다.", () => {
+    cy.get(".load-more").should("exist").click();
+    cy.get(".item-list li").should("have.length", 40);
+  });
 });
