f39b9d95d26288756d8053044285f1b429768c51
Paik <gwbaik9717@icloud.com>
2024-06-04 08:08:47 +0900

    test: 401 에러에 대한 테스트 케이스 추가

--- cypress/e2e/Api.cy.ts
+++ cypress/e2e/Api.cy.ts
@@ -4,7 +4,7 @@ import Api from "../../src/js/domain/Api";
 describe("Api 기능 테스트", () => {
   it("영화 목록 API를 호출하면 20개씩 목록에 나타나야 한다.", () => {
     const currentPage = 1;
-    const url = Api.generateUrl(currentPage);
+    const url = Api.generatePopularMoviesUrl(currentPage);
 
     cy.request(url).as("movies");
 
@@ -24,6 +24,21 @@ describe("Api 기능 테스트", () => {
 
     expect(() => {
       cy.request(WRONG_URL).should("throw", ErrorMessage.NOT_VALID_URL);
+    });
+  });
+
+  it("인증되지 않은 API 키로 영화 목록 API를 호출하면 에러를 발생시킨다.", () => {
+    const WRONG_API = "https://api.themoviedb.org/3/movie/popular";
+
+    cy.intercept(WRONG_API, {
+      statusCode: 401,
+      body: {
+        status_message: "Invalid API key: You must be granted a valid key.",
+      },
+    }).as("movies");
+
+    expect(() => {
+      cy.request(WRONG_API).should("throw", ErrorMessage.NOT_VALID_API_KEY);
     });
   });
 });
