05b8bd2be6d90dbfa3ed800f329f39996db9ef4b
Donghoon Kang <d159123@naver.com>
2024-06-03 23:27:20 +0900

    style: css 렌더링 순서 변경

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./css/common.css";
 import "./css/reset.css";
+import "./css/common.css";
 import { Controller } from "./js/controller";
 
 addEventListener("DOMContentLoaded", async () => {
-  const controller = new Controller;
+  const controller = new Controller();
   controller.init();
 });
