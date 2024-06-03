6d8886e36dffddead41d07a4cfa24e0ff1920b4b
seungdeok <akk9132@naver.com>
2024-06-03 23:57:43 +0900

    feat: add skeleton ui

--- src/views/MovieView.js
+++ src/views/MovieView.js
@@ -13,29 +13,56 @@ export class MovieView {
     this.loadMore();
   }
 
-  async loadMore() {
-    const movies = await this.movieInstance.loadMore();
-    console.log(movies);
-    movies.forEach((movie) => {
-      const movieElement = document.createElement("li");
-      const thumbnail = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
-      movieElement.innerHTML = /*html */ `
-            <a href="#">
-                <div class="item-card">
-                    <img class="item-thumbnail" loading="lazy" src="${thumbnail}" alt="${movie.title}">
-                    <p class="item-title">${movie.title}</p>
-                    <p class="item-score">
-                      <img src=${starFilled} alt="별점" />
-                      ${movie.vote_average}
-                    </p>
-                </div>
-            </a>
+  showSkeleton() {
+    for (let i = 0; i < 20; i++) {
+      const skeletonItem = document.createElement("li");
+      skeletonItem.classList.add("skeleton-item");
+      skeletonItem.innerHTML = /*html */ `
+            <div class="item-card">
+                <div class="item-thumbnail skeleton"></div>
+                <div class="item-title skeleton"></div>
+                <div class="item-score skeleton"></div>
+            </div>
         `;
-      this.itemList.appendChild(movieElement);
-    });
+      this.itemList.appendChild(skeletonItem);
+    }
+  }
 
-    if (!this.movieInstance.hasMore) {
-      this.loadMoreButton.style.display = "none";
+  hideSkeleton() {
+    this.itemList.querySelectorAll(".skeleton-item").forEach((skeleton) => {
+      skeleton.remove();
+    });
+  }
+
+  async loadMore() {
+    try {
+      this.showSkeleton();
+      const movies = await this.movieInstance.loadMore();
+      this.hideSkeleton();
+
+      movies.forEach((movie) => {
+        const movieElement = document.createElement("li");
+        const thumbnail = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+        movieElement.innerHTML = /*html */ `
+              <a href="#">
+                  <div class="item-card">
+                      <img class="item-thumbnail" loading="lazy" src="${thumbnail}" alt="${movie.title}">
+                      <p class="item-title">${movie.title}</p>
+                      <p class="item-score">
+                        <img src=${starFilled} alt="별점" />
+                        ${movie.vote_average}
+                      </p>
+                  </div>
+              </a>
+          `;
+        this.itemList.appendChild(movieElement);
+      });
+
+      if (!this.movieInstance.hasMore) {
+        this.loadMoreButton.style.display = "none";
+      }
+    } catch {
+      this.hideSkeleton();
     }
   }
 }
