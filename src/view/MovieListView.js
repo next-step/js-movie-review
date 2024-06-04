f2c077b9b8b6112f91918f4caae50ad3ed78ab7b
ChanwooPark <chanw9503@gmail.com>
2024-06-05 01:58:48 +0900

    Feat. 영화 리스트, 더보기, 검색 관련 내용 추가

---
+++ src/view/MovieListView.js
@@ -0,0 +1,47 @@
+import { Cinema } from '../components/Cinema';
+import { Modal } from '../components/modal';
+import getMovieList from '../apis/getMovieList';
+import getSearchedMovies from '../apis/getSearchEdMovies';
+
+export function MovieListView() {
+    const cinema = new Cinema();
+    const moreButton = document.getElementById('more-movies');
+    const searchInput = document.getElementById('search-input');
+
+    async function renderMovieItems(pages) {
+        try {
+            cinema.showSekeleton(20, true);
+            const itemList = await getMovieList(pages);
+            cinema.createMovies(itemList);
+        } catch (error) {
+            const modal = new Modal();
+            modal.content = error.message;
+            document.querySelector('body').appendChild(modal.rendered);
+        }
+    }
+    renderMovieItems(cinema.page);
+
+    moreButton.addEventListener('click', () => {
+        cinema.page += 1;
+        renderMovieItems(cinema.page);
+    });
+
+    searchInput.addEventListener('keydown', async (event) => {
+        if (event.code === 'Enter') {
+            try {
+                const { value } = searchInput;
+                const titleElement = document.querySelector('.item-view h2');
+                cinema.movieListElement.innerHTML = '';
+                titleElement.innerText = `"${value}" 검색 결과`;
+                cinema.showSekeleton(20, true);
+
+                const itemList = await getSearchedMovies(value);
+                cinema.createMovies(itemList.results);
+            } catch (error) {
+                const modal = new Modal();
+                modal.content = error.message;
+                document.querySelector('body').appendChild(modal.rendered);
+            }
+        }
+    });
+}
