80d845bd5582fa292ccaa00cbc8d82b90c4d8928
sochong <hsju0202@gmail.com>
2024-06-04 18:44:44 +0900

    test: 영화 목록 API를 호출하면 20개씩 목록에 나타나야한다

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("영화 목록 API를 호출하면 20개씩 목록에 나타나야한다.", () => {
+    const baseUrl = "https://api.themoviedb.org/3/movie/popular";
+    const param = new URLSearchParams({
+      language: "ko-KR",
+    });
+
+    cy.request({
+      url: `${baseUrl}?${param}`,
+      auth: {
+        bearer: Cypress.env("API_KEY"),
+      },
+    }).as("movies");
+
+    cy.get("@movies").its("status").should("eq", 200);
+    cy.get("@movies").its("body.results").should("have.length", 20);
+  });
+});
