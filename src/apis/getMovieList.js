b24ffd43fc49113105dffb14aa66fa851979e8a1
ChanwooPark <chanw9503@gmail.com>
2024-06-07 18:06:51 +0900

    Feat. API 공통 모듈분리에 따른 수정

--- src/apis/getMovieList.js
+++ src/apis/getMovieList.js
@@ -1,25 +1,13 @@
-import { BASE_URL } from '../constants/api';
-import { APIError } from './error';
-import { createUrlSeacrhParams } from './utils/createUrlSeachParams';
+import ApiClient from './\bapiClient';
 
 async function getMovieList(page) {
-    const url = createUrlSeacrhParams({
-        baseUrl: BASE_URL,
-        params: { api_key: process.env.TMDB_API_KEY, page: page, language: 'ko-KR' }
+    const res = ApiClient.request('GET', '3/movie/popular', {
+        api_key: process.env.TMDB_API_KEY,
+        page: page,
+        language: 'ko-KR'
     });
 
-    const res = await fetch(url, {
-        method: 'GET'
-    })
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            if (data.success !== undefined && !data.success) throw new APIError(data.status_code, data.status_message);
-            return data;
-        });
-
-    return res.results;
+    return res;
 }
 
 export default getMovieList;
