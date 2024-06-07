a0ee8e1c06ddab866f5ece842cf220f2b2e4edf1
ChanwooPark <chanw9503@gmail.com>
2024-06-07 18:08:11 +0900

    Feat. Cypress env 추가하기

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         }
+    },
+
+    env: {
+        TMDB_API_KEY: 'cd45ff31f728c6222a2830fc1fb7f44e'
     }
 });
