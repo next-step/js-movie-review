0a9c856ee70a92559365fab82fbae15930f15b69
ChanwooPark <chanw9503@gmail.com>
2024-06-07 22:02:08 +0900

    Feat. 검색 버튼 클릭시 검색되는 기능 추가

--- src/view/MovieListView.js
+++ src/view/MovieListView.js
@@ -7,6 +7,7 @@ export function MovieListView() {
     const cinema = new Cinema();
     const moreButton = document.getElementById('more-movies');
     const searchInput = document.getElementById('search-input');
+    const searchButton = document.querySelector('.search-button');
 
     async function renderMovieItems(pages) {
         try {
@@ -42,6 +43,23 @@ export function MovieListView() {
                 modal.content = error.message;
                 document.querySelector('body').appendChild(modal.rendered);
             }
+        }
+    });
+
+    searchButton.addEventListener('click', async () => {
+        try {
+            const { value } = searchInput;
+            const titleElement = document.querySelector('.item-view h2');
+            cinema.movieListElement.innerHTML = '';
+            titleElement.innerText = `"${value}" 검색 결과`;
+            cinema.showSekeleton(20, true);
+
+            const itemList = await getSearchedMovies(value);
+            cinema.createMovies(itemList.results);
+        } catch (error) {
+            const modal = new Modal();
+            modal.content = error.message;
+            document.querySelector('body').appendChild(modal.rendered);
         }
     });
 }
