678dcba4a4f947b85a24d2867c360b873653eb2b
ChanwooPark <chanw9503@gmail.com>
2024-06-07 18:04:57 +0900

    Rename. 폴더 위치변경

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../styles/reset.css';
-import '../styles/common.css';
-
-import { Layout } from '../components/Layout';
-import { MovieListView } from '../view/movieListView';
-
-const app = document.getElementById('app');
-app.appendChild(Layout());
-app.addEventListener('DOMContentLoaded', MovieListView());
