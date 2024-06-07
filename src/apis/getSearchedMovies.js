97cc34c515d9fe5158284582203be534e7f77fb5
ChanwooPark <chanw9503@gmail.com>
2024-06-07 22:01:16 +0900

    Fix. 검색 URL 변경

--- src/apis/getSearchedMovies.js
+++ src/apis/getSearchedMovies.js
@@ -1,8 +1,7 @@
-import { BASE_URL } from '../constants/api';
 import ApiClient from './\bapiClient';
 
 async function getSearchedMovies(query) {
-    const res = ApiClient.request('GET', BASE_URL, {
+    const res = ApiClient.request('GET', '3/search/movie', {
         query: query,
         api_key: process.env.TMDB_API_KEY,
         language: 'ko-KR'
