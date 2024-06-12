e996d71c5e956880c55e703e348e01daaf1f545b
ChanwooPark <chanw9503@gmail.com>
2024-06-13 02:08:36 +0900

    Feat. 돔조작 최소화 (Header)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
-function Header(logo, searchBox) {
-    const headerElement = document.createElement('header');
-    headerElement.classList.add('header');
+import Input from './Input';
+import Button from './Button';
 
-    headerElement.appendChild(logo);
-    headerElement.appendChild(searchBox);
+function Header({ logoSrc, logoAlt, onSearch }) {
+    const headerElement = document.createElement('header');
+
+    headerElement.innerHTML = `
+        <h1><img src="${logoSrc}" alt="${logoAlt}" loading="lazy"></h1>
+        <div class="search-box"></div>
+    `;
+
+    const searchInput = Input({
+        id: 'search-input',
+        type: 'text',
+        placeholder: '검색',
+        onKeyDown: async (event) => {
+            if (event.code === 'Enter') {
+                await onSearch(event.target.value);
+            }
+        }
+    });
+
+    const searchButton = Button({
+        classNames: ['search-button'],
+        name: '검색',
+        type: 'button',
+        onClick: async () => {
+            const inputValue = document.getElementById('search-input').value;
+            await onSearch(inputValue);
+        }
+    });
+
+    const searchBox = headerElement.querySelector('.search-box');
+    searchBox.appendChild(searchInput);
+    searchBox.appendChild(searchButton);
 
     return headerElement;
 }
