dc8532e092f33098639d06644a3b9361aa85c02b
ChanwooPark <chanw9503@gmail.com>
2024-06-16 14:14:58 +0900

    Design. Modal & responsive css 분리 및 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './styles/reset.css';
 import './styles/common.css';
+import './styles/modal.css';
+import './styles/responsive.css';
 import App from './view/App';
 
 document.addEventListener('DOMContentLoaded', () => {
