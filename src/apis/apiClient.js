34fd36cd25c89dccb0a2d0718b4707760eabf711
ChanwooPark <chanw9503@gmail.com>
2024-06-16 14:12:13 +0900

    Fix. fetch 에러 발생시  Fetch 에러 사용

--- src/apis/apiClient.js
+++ src/apis/apiClient.js
@@ -2,7 +2,7 @@ import { BASE_URL } from '../constants/api';
 import { APIError } from './error';
 
 class ApiClient {
-    static async request(method, endpoint, params, headers = {}, body = null) {
+    static async request({ method, endpoint, params, headers = {}, body = null }) {
         const url = params ? this.createUrlSearchParams(`${BASE_URL}/${endpoint}`, params) : `${BASE_URL}/${endpoint}`;
 
         const options = {
@@ -19,12 +19,11 @@ class ApiClient {
             const data = await response.json();
 
             if (!response.ok) {
-                console.log('data', data.status_code, data.status_message);
                 throw new APIError(data.status_code, data.status_message);
             }
             return data;
         } catch (error) {
-            throw new APIError(400, '데이터 요청에 실패했습니다. 다음에 다시 이용해주세요.');
+            throw new APIError(400, error.message);
         }
     }
 
