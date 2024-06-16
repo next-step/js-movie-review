5e98ea7508391a62743571cebbbc8a1f9a0f3884
ChanwooPark <chanw9503@gmail.com>
2024-06-16 18:03:52 +0900

    Feat. 더보기 클릭시 내용이 없을 경우, 버튼삭제 및 모달 띄우기

--- src/services/handleMovie.js
+++ src/services/handleMovie.js
@@ -1,6 +1,7 @@
 import getMovieList from '../apis/getMovieList';
 import ConfirmModal from '../components/modal/ConfirmModal';
 import { Modal } from '../components/modal/container/Modal';
+import { NOT_MORE_MOVIES_MESSAGE } from '../constants/message';
 import renderMovies from '../view/renderMovies';
 import renderSkeletonMovies from '../view/renderSkeletonMovies';
 
@@ -9,6 +10,12 @@ export default async function handleMovie(page) {
         renderSkeletonMovies({ loading: true });
         const movieList = await getMovieList(page);
         renderSkeletonMovies({ loading: false });
+
+        if (movieList.results.length === 0) {
+            document.getElementById('more-movies').remove();
+            new Modal(document.querySelector('body'), ConfirmModal(NOT_MORE_MOVIES_MESSAGE));
+        }
+
         renderMovies({ movieList: movieList.results, resetHTML: false });
     } catch (error) {
         renderSkeletonMovies({ loading: false });
--- src/services/handleSeach.js
+++ src/services/handleSeach.js
@@ -1,6 +1,7 @@
 import getSearchedMovies from '../apis/getSearchedMovies';
 import ConfirmModal from '../components/modal/ConfirmModal';
 import { Modal } from '../components/modal/container/Modal';
+import { NOT_MORE_MOVIES_MESSAGE } from '../constants/message';
 import renderMovies from '../view/renderMovies';
 import renderSkeletonMovies from '../view/renderSkeletonMovies';
 
@@ -11,6 +12,11 @@ export async function handleSearch(value, page = 1) {
         titleElement.innerText = `"${value}" 검색 결과`;
         const searchList = await getSearchedMovies(value, page);
         renderSkeletonMovies({ loading: false });
+
+        if (searchList.results.length === 0) {
+            document.getElementById('more-movies').remove();
+            new Modal(document.querySelector('body'), ConfirmModal(NOT_MORE_MOVIES_MESSAGE));
+        }
         renderMovies({ movieList: searchList.results, resetHTML: page === 1 ? true : false });
     } catch (error) {
         renderSkeletonMovies({ loading: false });
