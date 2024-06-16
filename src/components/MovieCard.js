aa9b2e184904ba2c723a8d36a4d45925aa37e0e0
ChanwooPark <chanw9503@gmail.com>
2024-06-16 14:13:38 +0900

    Feat. Card 클릭시 Detail Modal 띄우기

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
-export function MovieCard({ title = '', src = '', voteAvg = '', starSrc = '', loading = false }) {
+import getMovieDetail from '../apis/getMovieDetail';
+import MovieDetailModal from './modal/MovieDetailModal';
+import { Modal } from './modal/container/Modal';
+
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+export function MovieCard({ title = '', src = '', voteAvg = '', starSrc = '', id = 0, loading = false }) {
     if (loading) {
         return SkeletonMovieCard();
     }
 
     const liElement = document.createElement('li');
-
     const newVoteAvg = loading ? '' : Number(voteAvg).toFixed(2);
     const imageSrc = loading ? '' : src;
     const imageAlt = title;
@@ -21,6 +26,22 @@ export function MovieCard({ title = '', src = '', voteAvg = '', starSrc = '', lo
             </div>
         </a>
     `;
+
+    liElement.addEventListener('click', async () => {
+        const res = await getMovieDetail(id);
+
+        console.log('res', res);
+        new Modal(
+            document.querySelector('body'),
+            MovieDetailModal({
+                title: res.title,
+                src: `${IMAGE_BASE_URL}${res.poster_path}`,
+                genres: res.genres,
+                overview: res.overview,
+                vote_average: res.vote_average
+            })
+        );
+    });
 
     return liElement;
 }
