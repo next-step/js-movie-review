0315a72d8f96aa61d1570fb1f91797eae25f3cba
ChanwooPark <chanw9503@gmail.com>
2024-06-16 18:04:36 +0900

    Test. 테스트 추가하기

--- cypress/e2e/Searh.cy.js
+++ cypress/e2e/Searh.cy.js
@@ -34,4 +34,28 @@ describe('검색 관련된 내용 추가', () => {
 
         cy.get('.item-view h2').should('contain.text', '"혹성" 검색 결과');
     });
+
+    it('더보기 버튼 클릭 시, 결과값이 없으면 더보기 버튼을 삭제한다.', () => {
+        cy.visit('http://localhost:8080');
+
+        cy.get('#search-input').type('혹성');
+
+        cy.get('.search-button').click();
+        cy.get('#more-movies').click();
+
+        cy.get('#more-movies').should('not.exist');
+    });
+
+    it('더보기 버튼 클릭 시, 결과값이 없으면 모달창을 띄워 사용자에게 알려준다.', () => {
+        cy.visit('http://localhost:8080');
+
+        cy.get('#search-input').type('혹성');
+
+        cy.get('.search-button').click();
+        cy.get('#more-movies').click();
+
+        cy.get('.modal').should('be.visible');
+
+        cy.contains('.modal-content', '더이상 영화가 존재하지 않습니다.');
+    });
 });
