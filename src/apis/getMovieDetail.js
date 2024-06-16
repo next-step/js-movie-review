d23b0a63a44645467da99316d4c0a1c129d517a5
ChanwooPark <chanw9503@gmail.com>
2024-06-16 14:13:51 +0900

    Feat. Detail 데이터 페칭 추가

---
+++ src/apis/getMovieDetail.js
@@ -0,0 +1,16 @@
+import ApiClient from './apiClient';
+
+const getMovieDetail = async (movie_id) => {
+    const res = ApiClient.request({
+        method: 'GET',
+        endpoint: `3/movie/${movie_id}`,
+        params: {
+            api_key: process.env.TMDB_API_KEY,
+            language: 'ko-KR'
+        }
+    });
+
+    return res;
+};
+
+export default getMovieDetail;
