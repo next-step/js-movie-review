4386866991b804168d6447fdd02bdd2e2c1c3934
ChanwooPark <chanw9503@gmail.com>
2024-06-16 17:29:19 +0900

    Fix. 검색 내용 호출 시 페이지 정보 추가

--- src/services/handleSeach.js
+++ src/services/handleSeach.js
@@ -1,17 +1,17 @@
-import getSearchedMovies from '../apis/getSearchEdMovies';
+import getSearchedMovies from '../apis/getSearchedMovies';
 import ConfirmModal from '../components/modal/ConfirmModal';
 import { Modal } from '../components/modal/container/Modal';
 import renderMovies from '../view/renderMovies';
 import renderSkeletonMovies from '../view/renderSkeletonMovies';
 
-export async function handleSearch(value) {
+export async function handleSearch(value, page = 1) {
     try {
         renderSkeletonMovies({ loading: true });
         const titleElement = document.querySelector('.item-view h2');
         titleElement.innerText = `"${value}" 검색 결과`;
-        const searchList = await getSearchedMovies(value);
+        const searchList = await getSearchedMovies(value, page);
         renderSkeletonMovies({ loading: false });
-        renderMovies({ movieList: searchList.results, resetHTML: true });
+        renderMovies({ movieList: searchList.results, resetHTML: page === 1 ? true : false });
     } catch (error) {
         renderSkeletonMovies({ loading: false });
         new Modal(document.querySelector('body'), ConfirmModal(error.message));
