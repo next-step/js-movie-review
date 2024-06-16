ef132713e96ea26e8ef60368141b8676cf072ea0
ChanwooPark <chanw9503@gmail.com>
2024-06-16 14:14:07 +0900

    Fix. 매개변수 객체로 받도록 변경

--- src/apis/getMovieList.js
+++ src/apis/getMovieList.js
@@ -1,10 +1,14 @@
 import ApiClient from './apiClient';
 
 async function getMovieList(page) {
-    const res = ApiClient.request('GET', '3/movie/popular', {
-        api_key: process.env.TMDB_API_KEY,
-        page: page,
-        language: 'ko-KR'
+    const res = ApiClient.request({
+        method: 'GET',
+        endpoint: '3/movie/popular',
+        params: {
+            api_key: process.env.TMDB_API_KEY,
+            page: page,
+            language: 'ko-KR'
+        }
     });
 
     return res;
