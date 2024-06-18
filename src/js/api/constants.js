3f7f8616b9f145b5cfde845b1fcfef6a80f6f4da
Donghoon Kang <d159123@naver.com>
2024-06-18 21:30:12 +0900

    feat: 에러 dialog cloase 기능 추가

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,16 +1,16 @@
 export const BASE_URL = "https://api.themoviedb.org/3/movie/popular";
 
 export const ERROR_MSG = {
-  INVALID_API_KEY: { 
-    message: "API키가 유효하지 않습니다.", 
-    details: "API키를 확인하신 후 다시 시도해주세요."
+  INVALID_API_KEY: {
+    message: "API키가 유효하지 않습니다.",
+    details: "API키를 확인하신 후 다시 시도해주세요.",
   },
   INVALID_REQUEST: {
     message: "요청이 올바르지 않습니다.",
-    details: "API 요청 URL을 확인해주세요."
+    details: "API 요청 URL을 확인해주세요.",
   },
   DEFAULT: {
     message: "알 수 없는 오류가 발생했습니다.",
-    details: "잠시 후 다시 시도해주세요."
+    details: "잠시 후 다시 시도해주세요.",
   },
 };
--- src/js/components/main/error/error-dialog.js
+++ src/js/components/main/error/error-dialog.js
@@ -1,24 +1,32 @@
 export const errorDialog = {
-    render({message, details}) {
-        const element = document.createElement("div");
-        element.classList.add("error-dialog");
-        element.insertAdjacentHTML(
-            "afterbegin",
-            /*html*/ `
+  render({ message, details = "" }) {
+    const element = document.createElement("div");
+    element.classList.add("error-dialog");
+    element.insertAdjacentHTML(
+      "afterbegin",
+      /*html*/ `
             <div class="error-dialog-body">
                 <h1>${message}</h1>
                 <p>${details}</p>
                 <button class="retry-button">확인</button>
           </div>
             `
-        );
+    );
 
-        return element;
-    },
+    element.querySelector(".retry-button").addEventListener("click", () => {
+      this.close(element);
+    });
 
-    load(error) {
-        const main = document.querySelector("main");
-        const errorModal = this.render(error);
-        main.appendChild(errorModal);
-    }
-}
+    return element;
+  },
+
+  load(error) {
+    const main = document.querySelector("main");
+    const errorModal = this.render(error);
+    main.appendChild(errorModal);
+  },
+
+  close(element) {
+    element.remove();
+  },
+};
