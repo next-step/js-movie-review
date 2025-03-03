db67330875b4e3e226ebc1154516906a8d38af6c
JINJU KIM <devpearlkim@gmail.com>
2025-03-03 12:44:37 +0900

    test: Add skeleton UI and tab functionality tests

--- cypress/e2e/movies.cy.js
+++ cypress/e2e/movies.cy.js
@@ -1,14 +1,54 @@
-describe("Fetch Movie data", () => {
+describe("Movie App E2E Tests", () => {
   beforeEach(() => {
+    cy.intercept("GET", "**/popular?*", {
+      fixture: "popularMovies.json",
+    }).as("getPopularMovies");
+    cy.intercept("GET", "**/now_playing?*", {
+      fixture: "nowPlayingMovies.json",
+    }).as("getNowPlayingMovies");
+
     cy.visit("http://localhost:5173");
 
     cy.get("#movie-list-container").as("movieContainer");
   });
 
-  it("displays the skeleton UI on initial load", () => {
-    cy.get("@movieContainer")
-      .find(".skeleton")
-      .should("exist")
-      .and("be.visible");
+  describe("Skeleton UI Tests", () => {
+    it("shows skeleton UI on initial load", () => {
+      cy.get("@movieContainer").find(".skeleton").should("exist");
+
+      cy.wait("@getPopularMovies");
+      cy.get(".skeleton").should("not.exist");
+    });
+  });
+
+  describe("Tabs Functionality Tests", () => {
+    it("selects 'popular' tab by default", () => {
+      cy.get("[data-category='popular'] .tab-item").should(
+        "have.class",
+        "selected"
+      );
+    });
+
+    it("changes tab on click and remembers selection", () => {
+      cy.get("[data-category='now_playing']").click();
+      cy.get("[data-category='now_playing'] .tab-item").should(
+        "have.class",
+        "selected"
+      );
+
+      cy.reload();
+      cy.get("[data-category='now_playing'] .tab-item").should(
+        "have.class",
+        "selected"
+      );
+    });
+
+    it("load movies via selected tab changes", () => {
+      cy.get("[data-category='popular']").click();
+      cy.wait("@getPopularMovies");
+
+      cy.get("[data-category='now_playing']").click();
+      cy.wait("@getNowPlayingMovies");
+    });
   });
 });
