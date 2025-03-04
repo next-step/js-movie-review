5c9606fe237dfd7613e1edd5730130cb1fe5245a
Terry <kdjtiger@gmail.com>
2025-03-04 23:43:41 +0900

    feat: movieItem 추가

---
+++ src/components/MovieItem.js
@@ -0,0 +1,23 @@
+export const MovieItem = (props) => {
+  const { title, poster_path, vote_average } = props;
+
+  const rate = vote_average.toFixed(1);
+
+  return /*html*/ `
+    <li>
+        <div class="item">
+            <img
+                class="thumbnail"
+                src="https://media.themoviedb.org/t/p/w440_and_h660_face${poster_path}"
+                alt="${title}"
+            />
+            <div class="item-desc">
+                <p class="rate">
+                    <img src="./images/star_empty.png" class="star" />
+                    <span>${rate}</span>
+                </p>
+                <strong>${title}</strong>
+            </div>
+        </div>
+    </li>`;
+};
