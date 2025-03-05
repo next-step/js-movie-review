0bf607627b8b604c04d22f18a3db76850b595cc4
Terry <kdjtiger@gmail.com>
2025-03-06 00:33:42 +0900

    refactor: 더보기 버튼 렌더링 여부 변경

--- src/domain/MovieLoader.js
+++ src/domain/MovieLoader.js
@@ -6,6 +6,8 @@ import { MovieList, renderMovieItems } from "../components/MovieList";
 let currentPage = 1;
 let allMovies = [];
 let hasMoreMovies = true;
+const DEFAULT_MOVIE_RENDER_COUNT = 20;
+const MAX_PAGE = 500;
 
 export const initializeMovieSection = async () => {
   const initialMovies = await fetchPopularMovies();
@@ -18,11 +20,45 @@ export const initializeMovieSection = async () => {
   addLoadMoreButtonEvent();
 };
 
+const renderHeaders = (movie) => {
+  const wrap = document.querySelector("#wrap");
+  wrap.insertAdjacentHTML("afterbegin", Headers(movie));
+};
+
+const renderMovieSection = (movies) => {
+  const movieSection = document.querySelector(".movie-section");
+  movieSection.innerHTML = MovieList({ movies });
+
+  const initHasMoreMovies = movies.length === DEFAULT_MOVIE_RENDER_COUNT;
+  const loadMoreButtonHTML = LoadMoreButton(initHasMoreMovies);
+  movieSection.insertAdjacentHTML("beforeend", loadMoreButtonHTML);
+};
+
 const loadMoreMovies = async () => {
   if (!hasMoreMovies) return;
 
   const newMovies = await fetchPopularMovies(currentPage + 1);
-  if (newMovies.results.length === 0) {
+
+  allMovies = [...allMovies, ...newMovies.results];
+  currentPage++;
+
+  addMovieItems(newMovies.results);
+
+  updateLoadMoreButtonDisplay(newMovies.results.length);
+};
+
+const addLoadMoreButtonEvent = () => {
+  const loadMoreButton = document.getElementById("load-more-button");
+  loadMoreButton?.addEventListener("click", loadMoreMovies);
+};
+
+const addMovieItems = (movies) => {
+  const movieSection = document.querySelector(".thumbnail-list");
+  movieSection.insertAdjacentHTML("beforeend", renderMovieItems(movies));
+};
+
+const updateLoadMoreButtonDisplay = (loadedMovieCount) => {
+  if (loadedMovieCount === 0 || currentPage === MAX_PAGE) {
     hasMoreMovies = false;
 
     const loadMoreButton = document.getElementById("load-more-button");
@@ -31,32 +67,4 @@ const loadMoreMovies = async () => {
     }
     return;
   }
-
-  allMovies = [...allMovies, ...newMovies.results];
-  currentPage++;
-
-  const movieSection = document.querySelector(".thumbnail-list");
-  movieSection.insertAdjacentHTML(
-    "beforeend",
-    renderMovieItems(newMovies.results),
-  );
-};
-
-const addLoadMoreButtonEvent = () => {
-  const loadMoreButton = document.getElementById("load-more-button");
-  loadMoreButton?.addEventListener("click", loadMoreMovies);
-};
-
-const renderMovieSection = (movies) => {
-  const movieSection = document.querySelector(".movie-section");
-  movieSection.innerHTML = MovieList({ movies });
-
-  const initHasMoreMovies = movies.length > 0;
-  const loadMoreButtonHTML = LoadMoreButton(initHasMoreMovies);
-  movieSection.insertAdjacentHTML("beforeend", loadMoreButtonHTML);
-};
-
-const renderHeaders = (movie) => {
-  const wrap = document.querySelector("#wrap");
-  wrap.insertAdjacentHTML("afterbegin", Headers(movie));
 };
