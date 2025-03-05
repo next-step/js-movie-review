89307b28c18ac3b6d57f83e675580a4b06a1727b
minji Kong <minzyaaaaaa@gmail.com>
2025-03-06 00:25:14 +0900

    Test: 영화 목록 조회

--- cypress/e2e/movie-list.cy.js
+++ cypress/e2e/movie-list.cy.js
@@ -23,3 +23,68 @@ describe("영화 목록 조회 테스트", () => {
     cy.get(".thumbnail-list > li .item-title").should("exist");
   });
 });
+
+describe("영화 목록 더보기 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+    cy.callMoviePopularList({ statusCode: 200 });
+  });
+
+  it("더보기 버튼을 누르면 영화 목록 20개가 추가로 로드된다.", () => {
+    cy.wait("@getPopularMovies");
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+
+    cy.get(".load-button").click();
+
+    cy.get(".thumbnail-list > li").should("have.length", 40);
+  });
+
+  it("영화 목록 추가 로드 시 스켈레톤 UI가 표시된다.", () => {
+    cy.get(".thumbnail-list > li.skeleton").should("not.exist");
+    cy.callMoviePopularList({ statusCode: 200, delay: 500 });
+
+    cy.get(".load-button").click();
+
+    cy.get("li.skeleton").should("have.length", 20);
+  });
+
+  it("목록 로딩이 완료되면 스켈레톤 UI가 사라진다.", () => {
+    cy.get(".thumbnail-list > li.skeleton").should("not.exist");
+
+    cy.get(".load-button").click();
+
+    cy.wait("@getPopularMovies");
+    cy.get(".thumbnail-list > li.skeleton").should("not.exist");
+  });
+});
+
+describe("영화 목록 더보기 데이터 로드 실패 상황 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+
+    cy.callMoviePopularList({ statusCode: 200 });
+    cy.wait("@getPopularMovies");
+
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+
+    cy.callMoviePopularList({ statusCode: 400, body: { success: false } });
+    cy.get(".load-button").click();
+    cy.wait("@getPopularMovies");
+  });
+
+  it("영화 목록 추가 로드 실패 시 폴백 UI를 표시한다.", () => {
+    cy.get(".thumbnail-list > li").should("have.length", 20);
+    cy.get(".fallback-message").should(
+      "have.text",
+      "데이터를 불러오는데 실패했습니다."
+    );
+  });
+
+  it("폴백 버튼 클릭 시 영화 목록을 다시 로드한다.", () => {
+    cy.callMoviePopularList({ statusCode: 200 });
+    cy.get(".fallback-button").click();
+    cy.wait("@getPopularMovies");
+
+    cy.get(".thumbnail-list > li").should("have.length", 40);
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add(
+  "callMoviePopularList",
+  ({ statusCode = 200, body, delay = 0 }) => {
+    return cy
+      .intercept(
+        {
+          method: "GET",
+          url: /^https:\/\/api\.themoviedb\.org\/3\/movie\/popular*/,
+        },
+        {
+          statusCode,
+          delay,
+          ...(body ? body : { fixture: "movie-popular.json" }),
+        }
+      )
+      .as("getPopularMovies");
+  }
+);
