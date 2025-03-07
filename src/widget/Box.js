d898ee259447073866b80f89788b3397c6fffc47
IMHOJEONG <39ghwjd@naver.com>
2025-03-08 00:19:06 +0900

    refactor: refactor with toElement

--- src/widget/Box.js
+++ src/widget/Box.js
@@ -1,14 +1,4 @@
-export const Box = () => {
-  const container = document.createDocumentFragment();
-  const div = document.createElement("div");
-  container.appendChild(div);
+import { toElement } from "../shared/ui";
 
-  const render = () => {
-    div.innerHTML = /* html */ `BOX`;
-  };
-
-  // 초기 렌더
-  render();
-
-  return container;
-};
+export const Box = () =>
+  toElement({ rootElementName: "div", domString: "BOX" });
