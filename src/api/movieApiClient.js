c0c9c319470b6005f2117db5d2fb0a0a18f2139a
IMHOJEONG <39ghwjd@naver.com>
2025-03-08 00:15:36 +0900

    fix: rename get -> movieApiClient

--- src/api/movieApiClient.js
+++ src/api/movieApiClient.js
@@ -6,9 +6,9 @@ export const getFavoriteMovies = async (index = 1) => {
       Authorization: `Bearer ${import.meta.env.VITE_MOVIE_TMDB_KEY}`,
     },
   });
-  const data = await response.json();
+  const { results } = await response.json();
 
-  return data;
+  return results;
 };
 
 export const getTopRatedMovies = async () => {
@@ -19,7 +19,7 @@ export const getTopRatedMovies = async () => {
       Authorization: `Bearer ${import.meta.env.VITE_MOVIE_TMDB_KEY}`,
     },
   });
-  const data = await response.json();
+  const { results } = await response.json();
 
-  return data;
+  return results;
 };
