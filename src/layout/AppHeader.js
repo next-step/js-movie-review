f8134e269d8c89c516d371d63caed015cc86cb38
IMHOJEONG <39ghwjd@naver.com>
2025-03-07 22:29:49 +0900

    fix: fix search icon

--- src/layout/AppHeader.js
+++ src/layout/AppHeader.js
@@ -45,9 +45,14 @@ export const AppHeader = ({ inputState }) => {
         <div class="logo-and-searchbox">
           <h1 class="logo">
             <img src="logo.png" alt="MovieList" />
-          </h1>
-          <input class="search" type="text"/>
-          <div></div>
+            </h1>
+            <div class="search-icon-box">
+              <input class="search" type="text"/>
+              <svg 
+                class="search-icon" 
+              xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+            </div>
+            <div></div>
         </div>
           <div class="top-rated-movie">
           
