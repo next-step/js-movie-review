f6112fd72d2be3958cbfe2d090066f5257f7f39a
minji Kong <minzyaaaaaa@gmail.com>
2025-03-09 21:44:09 +0900

    Refactor: 주석 상세화

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import {
 
 import { fetchApiWithPagination } from "src/shared/apis/api";
 
-// API 에러 메시지: "Invalid page: Pages start at 1 and max at 500. They are expected to be an integer."
+// API 제한사항: 페이지는 1부터 시작하며 최대 500까지만 가능합니다.
+// 500을 초과하는 페이지 요청 시 API 에러 발생: "Invalid page: Pages start at 1 and max at 500. They are expected to be an integer."
 const MAX_PAGE = 500;
 
 const fetchPopularMovies = async () => {
