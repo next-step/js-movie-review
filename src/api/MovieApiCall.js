96de476323f25d1c7c8683e6ad60a11e2c4a3212
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-10 16:53:40 +0900

    feat: API Call Function

---
+++ src/api/MovieApiCall.js
@@ -0,0 +1,24 @@
+import MovieListModel from "../domain/MovieListModel.js";
+
+const BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+const API_KEY = process.env.API_KEY;
+
+export async function movieApiCall(movieApiQuery) {
+  const apiUrl = `${BASE_URL}?${movieApiQuery.toQueryString()}`;
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+
+    return new MovieListModel(await response.json());
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
