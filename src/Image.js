65f2f6305905637a640e7cac1ab264816bcc567a
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-13 01:38:23 +0900

    feat: 이미지 public으로 이동

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,4 @@
 export const starImage = {
-  filled: "./templates/images/star_filled.png",
-  empty: "./templates/images/star_empty.png",
+  filled: "star_filled.png",
+  empty: "star_empty.png",
 };
