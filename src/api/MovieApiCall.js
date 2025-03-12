7d9c5b5546731bc8fc382ccd17250bb0b9217aab
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-03-13 01:42:55 +0900

    refactor: API 정보는 외부에서 함께 관리하도록 수정

--- src/api/MovieApiCall.js
+++ src/api/MovieApiCall.js
@@ -1,15 +1,13 @@
 import MovieListModel from "../domain/MovieListModel.js";
+import { movieApiUrl, movieApiKey } from "./MovieApiInfomation.js";
 
-const BASE_URL = "https://api.themoviedb.org/3/discover/movie";
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-export async function movieApiCall(movieApiQuery) {
-  const apiUrl = `${BASE_URL}?${movieApiQuery.toQueryString()}`;
+export async function getMovie(movieApiQuery) {
+  const apiUrl = `${movieApiUrl}?${movieApiQuery.toQueryString()}`;
   try {
     const response = await fetch(apiUrl, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        Authorization: "Bearer " + movieApiKey,
       },
     });
 
