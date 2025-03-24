2ef8684cdbe4a2a1756cba7b7c5ee694337e4e2e
IMHOJEONG <39ghwjd@naver.com>
2025-03-24 22:17:06 +0900

    feat: add intersection-observer

--- src/layout/AppMain.js
+++ src/layout/AppMain.js
@@ -7,6 +7,7 @@ import {
 } from "../api/movieApiClient";
 import { toElement } from "../shared/ui";
 import { eventEmitter, renderer } from "../shared/renderer";
+import { options } from "../shared/intersection-observer";
 
 export const AppMain = ({ inputState }) => {
   const [mainState, setState] = renderer.state("app-main", []);
@@ -14,7 +15,7 @@ export const AppMain = ({ inputState }) => {
 
   const fetchData = async (page) => {
     const data = await getFavoriteMovies(page);
-    setState([...data]);
+    setState([...mainState.value, ...data]);
   };
 
   fetchData(pageState.value);
@@ -26,9 +27,10 @@ export const AppMain = ({ inputState }) => {
           <h2>지금 인기 있는 영화</h2>  
           <section>
           </section>
-          ${inputState.value === "" ? '<button class="add-more">더보기</button>' : ""}
+          <div class="more"></div>
         </div>
       </main>`);
+
 
     const handleClick = () => {
       if (pageState.value >= 3) {
@@ -43,6 +45,27 @@ export const AppMain = ({ inputState }) => {
 
     const sectionElement = container.querySelector("section");
     sectionElement.firstChild.replaceWith(ThumbnailList(mainState.value))    
+  
+    const callback = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && entry.intersectionRatio > 0) {
+          // Element is in view, do something
+          console.log("Element is in view");
+          if (inputState.value !== "" || pageState.value >= 3) {
+            return;
+          }
+          setPageState(pageState.value + 1);
+          fetchData(pageState.value);
+        } else {
+          // Element is out of view, do something
+          console.log("Element is out of view");
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(container.querySelector('.more'));
+
 
     return container;
   };
