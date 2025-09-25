ce36eb985c81172f12d3ef275bea9d354aaef12a
강현석 <kekemusa37@gmail.com>
2025-09-26 00:25:45 +0900

    feat: 영화 아이템을 구현합니다.

---
+++ src/components/movie-item.js
@@ -0,0 +1,30 @@
+const THUMBNAIL_URL_PATH = "https://image.tmdb.org/t/p/w200";
+
+export const createMovieItem = (item) => {
+  const list = document.createElement("li");
+
+  const itemDivision = document.createElement("div");
+  itemDivision.classList.add("item");
+
+  const thumbnail = document.createElement("img");
+  thumbnail.src = `${THUMBNAIL_URL_PATH}${item.poster_path}`;
+
+  const rateDivision = document.createElement("div");
+  rateDivision.classList.add("item-desc");
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("rate");
+  const starIcon = document.createElement("img");
+  starIcon.classList.add("star");
+  starIcon.src = "./images/star_empty.png";
+  const rateSpan = document.createElement("span");
+  rateSpan.textContent = `${item.vote_average}`;
+  const title = document.createElement("strong");
+  title.textContent = `${item.title}`;
+
+  paragraph.append(starIcon, rateSpan);
+  rateDivision.append(paragraph, title);
+  itemDivision.append(thumbnail, rateDivision);
+  list.appendChild(itemDivision);
+
+  return list;
+};
