0538641407c2048996a4fffd6ad8da6181349c16
강현석 <kekemusa37@gmail.com>
2025-09-26 22:24:42 +0900

    feat: 더보기 버튼을 누르면 그 다음의 영화 목록을 불러올 수 있도록 합니다.

--- cypress/e2e/scenario.cy.js
+++ cypress/e2e/scenario.cy.js
@@ -1,13 +1,31 @@
 describe("사용자 시나리오", () => {
   beforeEach(() => {
+    cy.intercept("GET", "https://api.themoviedb.org/3/movie/popular?*page=1", {
+      fixture: "movie-popular-list.json",
+    }).as("getPopularMovies");
+    cy.intercept("GET", "https://api.themoviedb.org/3/movie/popular?*page=2", {
+      fixture: "more-movie-popular-list.json",
+    }).as("getPopularMoviesMore");
     cy.visit("http://localhost:5173");
   });
   it("사용자가 페이지에 진입하면 20개의 영화 포스터를 볼 수 있습니다", () => {
     // given
+    cy.wait("@getPopularMovies");
 
     // when
 
     // then
     cy.get(".thumbnail-list > li .item").should("have.length", 20);
+  });
+  it("사용자가 더보기 버튼을 누르면 그 다음의 영화 목록을 볼 수 있습니다.", () => {
+    // given
+    cy.wait("@getPopularMovies");
+
+    // when
+    cy.get(".more").click();
+    cy.wait("@getPopularMoviesMore");
+
+    // then
+    cy.get(".thumbnail-list > li .item").should("have.length", 40);
   });
 });
--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@ import { getPopularMovies } from "./services/movie-api.js";
 
 addEventListener("load", async () => {
   const popularMovieListData = await getPopularMovies();
-  const popularMovieList = createMovieList(popularMovieListData.results);
+  const movieList = popularMovieListData.results;
+  const popularMovieList = createMovieList(movieList);
+
+  const moreButton = document.querySelector("main .more");
+  if (popularMovieListData.total_pages > popularMovieListData.page) {
+    moreButton.classList.add("visible");
+  } else {
+    moreButton.classList.remove("visible");
+  }
+  moreButton.addEventListener("click", async () => {
+    const moreMovieListData = await getPopularMovies(
+      popularMovieListData.page + 1
+    );
+    const moreMovieList = createMovieList(moreMovieListData.results);
+
+    thumbnailList.appendChild(moreMovieList);
+  });
 
   const thumbnailList = document.querySelector("main .thumbnail-list");
   thumbnailList.classList.add("thumbnail-list");
