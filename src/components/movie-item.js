db0636d89fa25e7a1e5ad64541aa810641f76d8d
강현석 <kekemusa37@gmail.com>
2025-09-29 11:34:35 +0900

    review: component를 구현할 때 하나의 함수에는 하나의 append만 할 수 있도록 개선합니다.

--- src/components/movie-item.js
+++ src/components/movie-item.js
@@ -4,31 +4,61 @@ const THUMBNAIL_URL_PATH =
   "https://media.themoviedb.org/t/p/w440_and_h660_face/";
 
 export const createMovieItem = (item) => {
-  const list = document.createElement("li");
+  const listItem = document.createElement("li");
 
-  const itemDivision = document.createElement("div");
-  itemDivision.classList.add("item");
+  const itemContainer = createItemContainer({
+    thumbnailPath: item.poster_path,
+    voteAverage: item.vote_average,
+    title: item.title,
+  });
+
+  listItem.appendChild(itemContainer);
+
+  return listItem;
+};
+
+const createItemContainer = ({ thumbnailPath, voteAverage, title }) => {
+  const itemContainer = document.createElement("div");
+  itemContainer.classList.add("item");
 
   const thumbnail = document.createElement("img");
   thumbnail.className = "thumbnail";
-  thumbnail.src = `${THUMBNAIL_URL_PATH}${item.poster_path}`;
+  thumbnail.src = `${THUMBNAIL_URL_PATH}${thumbnailPath}`;
 
-  const rateDivision = document.createElement("div");
-  rateDivision.classList.add("item-desc");
+  const infoContainer = createInfoItem({
+    voteAverage: voteAverage,
+    title: title,
+  });
+
+  itemContainer.append(thumbnail, infoContainer);
+
+  return itemContainer;
+};
+
+const createInfoItem = ({ voteAverage, title: movieTitle }) => {
+  const container = document.createElement("div");
+  container.classList.add("item-desc");
+
+  const rate = createRateItem(voteAverage);
+
+  const title = document.createElement("strong");
+  title.textContent = movieTitle;
+
+  container.append(rate, title);
+
+  return container;
+};
+
+const createRateItem = (voteAverage) => {
   const paragraph = document.createElement("p");
   paragraph.classList.add("rate");
   const starIcon = document.createElement("img");
   starIcon.classList.add("star");
   starIcon.src = starImage;
   const rateSpan = document.createElement("span");
-  rateSpan.textContent = `${item.vote_average}`;
-  const title = document.createElement("strong");
-  title.textContent = `${item.title}`;
+  rateSpan.textContent = voteAverage;
 
   paragraph.append(starIcon, rateSpan);
-  rateDivision.append(paragraph, title);
-  itemDivision.append(thumbnail, rateDivision);
-  list.appendChild(itemDivision);
 
-  return list;
+  return paragraph;
 };
